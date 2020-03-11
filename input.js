7157c5f81054600272f5f96c973297250e74e2ff
vagrant <jayburbyga@gmail.com>
2020-03-11 19:00:48 +0000

    added say functionality

--- input.js
+++ input.js
@@ -9,10 +9,12 @@ const handleUserInput = (data) => {
   if (data === "w") connection.write('Move: up');
   if (data === "d") connection.write('Move: right');
   if (data === "s") connection.write('Move: down');
-  if (data === "j") connection.write('Move: left');
-  if (data === "i") connection.write('Move: up');
-  if (data === "l") connection.write('Move: right');
-  if (data === "k") connection.write('Move: down');
+
+  if (data === "b") connection.write('Say: beep beep');
+  if (data === "m") connection.write('Say: move b**ch,get out the way');
+  if (data === "v") connection.write('Say: Abada cadabra');
+  if (data === "h") connection.write('Say: Hello');
+  
   if (data === "\u0003") {
     console.log("See ya Later snakalator\n");
     process.exit();
