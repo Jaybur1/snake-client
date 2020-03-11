75ed07673d46f7b2d84a7a71c79d8d2f6af4cfe8
vagrant <jayburbyga@gmail.com>
2020-03-11 18:17:51 +0000

    added stdin setup and input handler

--- play.js
+++ play.js
@@ -1,5 +1,28 @@
 const { connect } = require('./client');
-const typewrite = require('./typewrite')
+const typewrite = require('./typewrite');
+
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = (data) => {
+  if (data === "\u0003") {
+    console.log("See ya Later snakalator\n");
+    process.exit();
+  }
+}
+ const setupInput = () => {
+   const stdin = process.stdin;
+   stdin.setRawMode(true);
+   stdin.setEncoding('utf8');
+   stdin.resume();
+   stdin.on('data', (key) => {
+    handleUserInput(key);
+   })
+   return stdin;
+ }
 
 console.log('Connecting...')
 connect();
+setupInput();
